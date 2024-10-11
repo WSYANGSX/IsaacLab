d11d35f7e90604230172b10bf60ecb9f6b6f45a3
WSYANGSX <1062033103@qq.com>
2024-10-11 19:21:14 +0800

    modified

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/franka_cabinet/__init__.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/franka_cabinet/__init__.py
@@ -16,7 +16,7 @@ from .franka_cabinet_env import FrankaCabinetEnv, FrankaCabinetEnvCfg
 ##
 
 gym.register(
-    id="Isaac-Franka-Cabinet-Direct-PPO-v0",
+    id="Isaac-Franka-Cabinet-Direct-v0",
     entry_point="omni.isaac.lab_tasks.direct.franka_cabinet:FrankaCabinetEnv",
     disable_env_checker=True,
     kwargs={
@@ -24,15 +24,5 @@ gym.register(
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:FrankaCabinetPPORunnerCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
-    },
-)
-
-gym.register(
-    id="Isaac-Franka-Cabinet-Direct-SAC-v0",
-    entry_point="omni.isaac.lab_tasks.direct.franka_cabinet:FrankaCabinetEnv",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": FrankaCabinetEnvCfg,
-        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_sac_cfg.yaml",
     },
 )
