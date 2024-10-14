06dbd0a8b589f0335443d972c6be8f6886cdc37e
WSYANGSX <1062033103@qq.com>
2024-10-14 20:18:57 +0800

    new file

---
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/manipulation/open_pick_place/config/franka/dense_reward_env_cfg.py
@@ -0,0 +1,90 @@
+# Copyright (c) 2022-2024, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+
+from omni.isaac.lab.sensors import FrameTransformerCfg
+from omni.isaac.lab.sensors.frame_transformer.frame_transformer_cfg import OffsetCfg
+from omni.isaac.lab.utils import configclass
+from omni.isaac.lab_tasks.manager_based.manipulation.open_pick_place import mdp
+from omni.isaac.lab_tasks.manager_based.manipulation.open_pick_place.open_pick_place_env_cfg import (
+    OpenPickPlaceEnvCfg,
+)
+
+##
+# Pre-defined configs
+##
+from omni.isaac.lab.markers.config import FRAME_MARKER_CFG  # isort: skip
+from omni.isaac.lab_assets.franka import FRANKA_PANDA_CFG  # isort: skip
+
+
+@configclass
+class DenseRewardEnvCfg(OpenPickPlaceEnvCfg):
+    def __post_init__(self):
+        # post init of parent
+        super().__post_init__()
+
+        # Set Franka as robot
+        self.scene.robot = FRANKA_PANDA_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+
+        # Set actions for the specific robot type (franka)
+        self.actions.arm_action = mdp.JointPositionActionCfg(
+            asset_name="robot",
+            joint_names=["panda_joint.*"],
+            scale=0.5,
+            use_default_offset=True,
+        )
+        self.actions.gripper_action = mdp.BinaryJointPositionActionCfg(
+            asset_name="robot",
+            joint_names=["panda_finger.*"],
+            open_command_expr={"panda_finger_.*": 0.04},
+            close_command_expr={"panda_finger_.*": 0.0},
+        )
+
+        # Listens to the required transforms
+        marker_cfg = FRAME_MARKER_CFG.copy()
+        marker_cfg.markers["frame"].scale = (0.1, 0.1, 0.1)
+        marker_cfg.prim_path = "/Visuals/FrameTransformer"
+
+        self.scene.ee_frame = FrameTransformerCfg(
+            prim_path="{ENV_REGEX_NS}/Robot/panda_link0",
+            debug_vis=True,
+            visualizer_cfg=marker_cfg,
+            target_frames=[
+                FrameTransformerCfg.FrameCfg(
+                    prim_path="{ENV_REGEX_NS}/Robot/panda_hand",
+                    name="end_effector",
+                    offset=OffsetCfg(
+                        pos=[0.0, 0.0, 0.1034],
+                    ),
+                ),
+            ],
+        )
+
+        self.scene.lid_frame = FrameTransformerCfg(
+            prim_path="{ENV_REGEX_NS}/Lid/lid",
+            debug_vis=True,
+            visualizer_cfg=marker_cfg,
+            target_frames=[
+                FrameTransformerCfg.FrameCfg(
+                    prim_path="{ENV_REGEX_NS}/Lid/lid",
+                    name="lid",
+                    offset=OffsetCfg(
+                        pos=[0.0, 0.0, 0.123],
+                    ),
+                ),
+            ],
+        )
+
+
+@configclass
+class DenseRewardEnvCfg_PLAY(OpenPickPlaceEnvCfg):
+    def __post_init__(self):
+        # post init of parent
+        super().__post_init__()
+        # make a smaller scene for play
+        self.scene.num_envs = 1
+        self.scene.env_spacing = 2.5
+        # disable randomization for play
+        self.observations.policy.enable_corruption = False
