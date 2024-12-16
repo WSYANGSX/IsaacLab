e91c2a080258b76d25493b6134410a7094e082dd
WSYANGSX <1062033103@qq.com>
2024-12-16 09:38:17 +0800

    modified

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/gbagc_cabient_opening/__init__.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/gbagc_cabient_opening/__init__.py
@@ -9,10 +9,10 @@ from . import agents
 # MLP Train
 gym.register(
     id="Isaac-Gbagc-Cabinet_Opening-Direct-v0",
-    entry_point=f"{__name__}.gbagc_cabinet_opening_env:GbagcCabinetOpeningEnv",
+    entry_point=f"{__name__}.gbagc_cabinet_opening_same_freq_env:GbagcCabinetOpeningEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.gbagc_cabinet_opening_env:GbagcCabinetOpeningEnvCfg",
+        "env_cfg_entry_point": f"{__name__}.gbagc_cabinet_opening_same_freq_env:GbagcCabinetOpeningEnvCfg",
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
     },
 )
