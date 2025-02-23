db059e83e779ec34de764124201490ac29c69bbe
WSYANGSX <1062033103@qq.com>
2025-02-23 17:48:44 +0800

    modified

--- my_projects/learning/create_an_empty_scene.py
+++ my_projects/learning/create_an_empty_scene.py
@@ -1,46 +1,61 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""This script demonstrates how to create a simple stage in Isaac Sim.
+
+.. code-block:: bash
+
+    # Usage
+    ./isaaclab.sh -p scripts/tutorials/00_sim/create_empty.py
+
+"""
+
+"""Launch Isaac Sim Simulator first."""
+
+
 import argparse
 
-from omni.isaac.lab.app import AppLauncher
-
-"""首先登录app"""
+from isaaclab.app import AppLauncher
 
 # create argparser
-parser = argparse.ArgumentParser(description="Tutorial 1")
-
+parser = argparse.ArgumentParser(description="Tutorial on creating an empty stage.")
+# append AppLauncher cli args
 AppLauncher.add_app_launcher_args(parser)
-
+# parse the arguments
 args_cli = parser.parse_args()
-
 # launch omniverse app
 app_launcher = AppLauncher(args_cli)
 simulation_app = app_launcher.app
 
-"""任务逻辑实现部分"""
+"""Rest everything follows."""
 
-from omni.isaac.lab.sim import SimulationCfg, SimulationContext
+from isaaclab.sim import SimulationCfg, SimulationContext
 
 
-# 主函数
 def main():
-    # 初始化仿真参数
-    sim_cfg = SimulationCfg(dt=0.01, use_fabric=True, substeps=2)
-    sim = SimulationContext(sim_cfg)
+    """Main function."""
 
-    # 设置摄像机
+    # Initialize the simulation context
+    sim_cfg = SimulationCfg(dt=0.01)
+    sim = SimulationContext(sim_cfg)
+    # Set main camera
     sim.set_camera_view([2.5, 2.5, 2.5], [0.0, 0.0, 0.0])
 
-    # 运行仿真
+    # Play the simulator
     sim.reset()
+    # Now we are ready!
     print("[INFO]: Setup complete...")
-    
-    # 时间步管理
-    while simulation_app.is_running():
-        # 执行物理步
-        sim.step()
-        
 
-if __name__=="__main__":
-    # 执行主函数
+    # Simulate physics
+    while simulation_app.is_running():
+        # perform step
+        sim.step()
+
+
+if __name__ == "__main__":
+    # run the main function
     main()
-    # 关闭sim app
+    # close sim app
     simulation_app.close()
