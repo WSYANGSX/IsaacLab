5397293141c2f226ff4b6cc953c1301925ced9b1
WSYANGSX <1062033103@qq.com>
2025-09-18 20:36:54 +0800

    new file

---
+++ source/isaaclab_tasks/isaaclab_tasks/direct/prtpr/__init__.py
@@ -0,0 +1,27 @@
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+gym.register(
+    id="Isaac-PRTPR-Direct-v0",
+    entry_point=f"{__name__}.prtpr_env:PrtprEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.prtpr_env_cfg:PrtprEnv",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+    },
+)
+
+gym.register(
+    id="Isaac-PRTPR-Direct-Lstm-v0",
+    entry_point=f"{__name__}.prtpr_env:PrtprEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.prtpr_env_cfg:PrtprEnv",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_lstm_cfg.yaml",
+    },
+)
